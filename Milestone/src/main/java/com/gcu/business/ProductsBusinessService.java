1650b7628f0a43bda1b84c4ff536b5496e2b261e
ktbloomq <ktb@bloomquist.com>
2023-03-31 14:00:21 -0700

    implement product system

---
+++ Milestone/src/main/java/com/gcu/business/ProductsBusinessService.java
@@ -0,0 +1,31 @@
+package com.gcu.business;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.gcu.model.ProductModel;
+
+public class ProductsBusinessService {
+
+    public void test() {
+        System.out.println("Hello from the OrdersBusinessService");
+    }
+
+    public List<ProductModel> getProducts() {
+        List<ProductModel> products = new ArrayList<ProductModel>();
+        products.add(new ProductModel(0, "Pixel 3a", 300));
+        products.add(new ProductModel(1, "Pixel 4a", 400));
+        products.add(new ProductModel(2, "Pixel 5a", 500));
+        products.add(new ProductModel(3, "Pixel 6a", 600));
+        return products;
+    }
+
+    public void init() {
+        System.out.println("Initializing ProductsBusinessService :)");
+    }
+
+    public void destroy() {
+        System.out.println("Destroying ProductsBusinessService :(");
+    }
+    
+}
---
+++ Milestone/src/main/java/com/gcu/business/ProductsRestService.java
@@ -0,0 +1,40 @@
+package com.gcu.business;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// import com.gcu.model.ProductList;
+import com.gcu.model.ProductModel;
+
+@RestController
+@RequestMapping("/service")
+public class ProductsRestService {
+
+    @Autowired
+    private ProductsBusinessService service;
+
+    @GetMapping(path="/getjson", produces={MediaType.APPLICATION_JSON_VALUE})
+    public List<ProductModel> getOrdersAsJson() {
+        return service.getProducts();
+    }
+
+    // @GetMapping(path="/getxml", produces={MediaType.APPLICATION_XML_VALUE})
+    // public ProductList getOrdersAsXml() {
+    //     ProductList list = new ProductList();
+    //     list.setProducts(service.getProducts());
+    //     return list;
+    // }
+
+    @PostMapping(path="/add")
+    public ProductModel addProduct(@RequestBody ProductModel product) {
+        System.out.printf("id:%d name:%s price:$%f%n", product.getId(), product.getName(), product.getPrice());
+        return product;
+    }
+}
