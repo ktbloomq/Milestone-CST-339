3c00da77d4c5bf8a3f6f867b7480de8d98248ff9
melichar-m <michael.melichar2@gmail.com>
2023-03-31 18:41:15 -0700

    fix for products 404

--- Milestone/src/main/java/com/gcu/controller/Product_Controller.java
+++ Milestone/src/main/java/com/gcu/controller/Product_Controller.java
@@ -24,7 +24,7 @@ public class Product_Controller {
     @Autowired 
     private ProductsBusinessService service;
 
-    @GetMapping("")
+    @GetMapping("/")
     public String productsPage(Model model) {
         List<ProductModel> products = service.getProducts();
         model.addAttribute("title", "Products");
