5fa3b333866a4c5afec3f2a5b6d251314cd29fd2
ktbloomq <ktb@bloomquist.com>
2023-04-12 16:15:30 -0700

    set id as long

--- Milestone/src/main/java/com/gcu/model/ProductModel.java
+++ Milestone/src/main/java/com/gcu/model/ProductModel.java
@@ -28,7 +28,7 @@ public class ProductModel {
     public long getId() {
         return id;
     }
-    public void setId(int id) {
+    public void setId(long id) {
         this.id = id;
     }
     public String getName() {
