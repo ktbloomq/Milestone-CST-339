384e3b1b0f210fa88205b7448bebc0fe0177c4d7
ktbloomq <ktb@bloomquist.com>
2023-04-25 18:56:49 -0700

    better api documentation

--- Milestone/src/main/java/com/gcu/business/RestService.java
+++ Milestone/src/main/java/com/gcu/business/RestService.java
@@ -19,6 +19,11 @@ import com.gcu.model.OrderModel;
 // import com.gcu.model.ProductList;
 import com.gcu.model.ProductModel;
 
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
 @RestController
 @RequestMapping("/service")
 public class RestService {
@@ -46,6 +51,12 @@ public class RestService {
     //     return list;
     // }
 
+    @ApiResponses(value = { 
+        @ApiResponse(responseCode = "200", description = "found product", 
+          content = { @Content(mediaType = "application/json", 
+            schema = @Schema(implementation = ProductModel.class)) }),
+        @ApiResponse(responseCode = "404", description = "product not found", 
+          content = @Content) })
     @GetMapping(path="/getproduct/{id}")
     public ResponseEntity<ProductModel> getProduct(@PathVariable long id) {
         if(productsService.exists(id)) {
@@ -65,23 +76,36 @@ public class RestService {
         return null;
     }
 
+    @ApiResponses(value = { 
+        @ApiResponse(responseCode = "200", description = "found product", 
+          content = { @Content(mediaType = "application/json", 
+            schema = @Schema(implementation = ProductModel.class)) }),
+        @ApiResponse(responseCode = "404", description = "product not found", 
+          content = @Content) })
     @PostMapping(path="/updateproduct")
-    public ProductModel updateProduct(@RequestBody ProductModel product) {
+    public ResponseEntity<ProductModel> updateProduct(@RequestBody ProductModel product) {
         System.out.printf("id:%d name:%s price:$%f%n", product.getId(), product.getName(), product.getPrice());
         if(productsService.exists(product.getId())) {
             boolean isSuccess = productsService.addProduct(new ProductEntity(product.getId(), product.getName(), product.getPrice()));
             if(isSuccess) {
-                return product;
+                return new ResponseEntity<ProductModel>(product, HttpStatus.OK);
             }
         }
-        return null;
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
+    @ApiResponses(value = { 
+        @ApiResponse(responseCode = "200", description = "found product", 
+          content = { @Content(mediaType = "application/json") }),
+        @ApiResponse(responseCode = "404", description = "product not found", 
+          content = @Content) })
     @GetMapping(path="/deleteproduct/{id}")
-    public void deleteProduct(@PathVariable long id) {
+    public ResponseEntity<?> deleteProduct(@PathVariable long id) {
         if(productsService.exists(id)) {
             productsService.deleteProduct(id);
+            return new ResponseEntity<>(HttpStatus.OK);
         }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @PostMapping(path="/addorder")
